4b9e37b7f180fff3a43b2a3d37f9903276169da2
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-02 21:44:53 +0530

    feat: setup reduxjs store

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+
+const store = configureStore({
+    reducer: {
+        users: usersReducer,
+        playlists: [],
+        songs: []
+    }
+})
+
+export { store };
