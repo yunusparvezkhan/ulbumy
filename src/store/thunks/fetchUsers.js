b696d73412feac46854b78fdc993fdfc04c62b7a
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-02 23:40:32 +0530

    feat: create fetchUsers thunk file for fetching users

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,11 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const res = await axios.get("http://lcoalhost:3010/users")
+
+    return res.data;
+});
+
+
+export { fetchUsers }
