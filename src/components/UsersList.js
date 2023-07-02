0dabd03911a023c231278fd2c933098a2ce28898
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-03 00:06:50 +0530

    feat: render list of users from server
    this commit uses the thunk file to get data from the server
    and prints them in the UsersList component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { fetchUsers } from '../store'
+import { useSelector, useDispatch } from 'react-redux'
 
 const UsersList = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch])
+
+    const users = useSelector((state) => {
+        return state.users;
+    })
+
+    const renderUsers = users.data.map((user) => {
+        return <div>{user.name}</div>
+    })
+
     return (
-        <div>UsersList</div>
+        <div>
+            <h2>Fetch Users</h2>
+            {renderUsers}
+        </div>
     )
 }
 
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -2,8 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
-    const res = await axios.get("http://lcoalhost:3010/users")
-
+    const res = await axios.get("http://localhost:3010/users")
     return res.data;
 });
 
