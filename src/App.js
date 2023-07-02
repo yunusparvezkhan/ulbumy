f3bacb53d0b88618434dd4d26b9c22f21d49cd00
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-02 21:32:07 +0530

    feat: initiate UsersList component for listing users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <h1 className="text-3xl font-bold underline" >App</h1>
+      <UsersList />
     </div>
   );
 }
