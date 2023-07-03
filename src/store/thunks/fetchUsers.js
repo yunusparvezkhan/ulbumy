64c2a419da4d51ee7f06688b5254e7b9683dd046
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-03 23:49:47 +0530

    feat(dev): add delay in fetchUser Thunk function

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,19 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const res = await axios.get("http://localhost:3010/users")
+
+    // Dev Only
+    await pause(1500);
+
     return res.data;
 });
+
+// Development phase functions
+
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    })
+}
 
 export { fetchUsers }
