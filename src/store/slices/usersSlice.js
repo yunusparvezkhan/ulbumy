785381aec64e24f0cec0ac474458f0604193cfbc
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-05 14:37:00 +0530

    cleanup: remove isLoading and err states from redux store
    The two states in usersLice in redux store, 'isLoading' and 'err' are of
    no use as loading screen states are refactored to component states. Hence
    this commit will remove these two states from the store, and also the updates
    to these states in extraReducers' functions

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -6,34 +6,16 @@ const usersSlice = createSlice({
     name: "users",
     initialState: {
         data: [],
-        isLoading: false,
-        err: null
     },
     extraReducers(builder) {
         // Reducers for fetchUser Thunk
-        builder.addCase(fetchUsers.pending, (state) => {
-            state.isLoading = true;
-        });
         builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            state.isLoading = false;
             state.data = action.payload;
-        });
-        builder.addCase(fetchUsers.rejected, (state, action) => {
-            state.isLoading = false;
-            state.err = action.error;
         });
 
         // Reducers for addUser Thunk
-        builder.addCase(addUser.pending, (state) => {
-            state.isLoading = true
-        });
         builder.addCase(addUser.fulfilled, (state, action) => {
-            state.isLoading = false;
             state.data.push(action.payload);
-        });
-        builder.addCase(addUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.err = action.error;
         });
     }
 })
