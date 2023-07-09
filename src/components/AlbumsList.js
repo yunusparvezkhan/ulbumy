691d0c0a696064bbc9f015953e6b8092b80282a4
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-09 23:57:07 +0530

    feat: access albums data with RTK

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useFetchAlbumsQuery } from '../store/apis/albumsApi'
 
 const AlbumsList = ({ user }) => {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+    console.log(data, error, isLoading);
+
     return (
         <div>
             Albums for {user.name}
