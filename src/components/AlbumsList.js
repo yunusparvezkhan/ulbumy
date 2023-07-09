519c603bd3179000ade3cdb2609b8e7d244e0cfd
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-10 00:43:37 +0530

    Revert "feat: access albums data with RTK"
    This reverts commit 691d0c0a696064bbc9f015953e6b8092b80282a4.

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,14 +1,29 @@
 import React from 'react'
+<<<<<<< HEAD
 import { useFetchAlbumsQuery } from '../store/apis/albumsApi'
-
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    console.log(data);
 
+    const renderAlbums = data.map((album) => {
+        return (
+            <div className='p-10 bg-gray-700'>
+                <span>{album.name}</span>
+            </div>
+        )
+    })
+
+=======
+
+const AlbumsList = ({ user }) => {
+>>>>>>> parent of 691d0c0... feat: access albums data with RTK
     return (
         <div>
-            Albums for {user.name}
+            Albums
+            <div className='flex flex-row'>
+                {/* {renderAlbums} */}
+            </div>
         </div>
     )
 }
