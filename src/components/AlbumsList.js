20ea275582af8f094a186033454ac9049cac8423
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-10 01:40:23 +0530

    feat: render albums fetching with rtk api

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useFetchAlbumsQuery } from '../store';
 
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    if (!isLoading && !error) {
+        const renderAlbums = data.map((album) => {
+            return (
+                <div className='p-10 m-3 bg-gray-700' key={album.id}>
+                    <span>{album.name}</span>
+                </div>
+            )
+        })
 
-    return (
+        return (
+            <div>
+                Albums
+                <div className='flex flex-row'>
+                    {renderAlbums}
+                </div>
+            </div>
+        )
+    } else if (!error) {
+        return (
+            <div>
+                Albums
+                <div className='p-5 items-center flex flex-col' >
+                    Loading...
+                </div>
+            </div>
+        )
+    } else {
         <div>
             Albums
-            <div className='flex flex-row'>
-                {/* {renderAlbums} */}
+            <div className='p-5 items-center flex flex-col'>
+                Error: {error}
             </div>
         </div>
-    )
+    }
 }
 
 export default AlbumsList
