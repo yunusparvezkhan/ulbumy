a51f805df1996f60d607fb33861d3b4cd08ade50
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-10 00:59:22 +0530

    bug: try to map albums, sts:fail

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,23 +1,17 @@
 import React from 'react'
-<<<<<<< HEAD
 import { useFetchAlbumsQuery } from '../store/apis/albumsApi'
+
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data);
-
     const renderAlbums = data.map((album) => {
         return (
-            <div className='p-10 bg-gray-700'>
+            <div className='p-10 bg-gray-700 m-3' >
                 <span>{album.name}</span>
             </div>
         )
     })
 
-=======
-
-const AlbumsList = ({ user }) => {
->>>>>>> parent of 691d0c0... feat: access albums data with RTK
     return (
         <div>
             Albums
