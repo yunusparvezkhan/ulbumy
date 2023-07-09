ebeb1ce75723b404e9b692d76da713edd961e6d1
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-09 23:07:16 +0530

    feat: setup RTK api
    This commit creates one rtk api in a dedicated directory
    and connects that api to the reduxjs store index file.
    This creates complete RTK setup to be used in future
    commits

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query'
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from './apis/albumsApi';
 
 const store = configureStore({
     reducer: {
         users: usersReducer,
-        playlists: [],
+        [albumsApi.reducerPath]: albumsApi.reducer,
         songs: []
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware)
     }
 })
+
+setupListeners(store.dispatch);
 
 export { store };
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/deleteUser';
+export { useFetchAlbumsQuery } from './apis/albumsApi';
