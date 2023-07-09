9b6ad2f3d6576c87ea43ca41ed350b688af268f0
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-09 17:45:00 +0530

    feat: create component to handle listing users

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AlbumsList = ({ user }) => {
+    return (
+        <div>
+            Albums for {user.name}
+        </div>
+    )
+}
+
+export default AlbumsList
