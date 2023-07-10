d8b364b45884bdc45063c64384df753aecd506e0
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-10 21:30:02 +0530

    feat: create add Album button inside the AlbumList component
    This commit will create Add Album button inside the AlbumList component and if
    clicked on the button, it invokes a handler function that console logs out
    the user's name, this button also creates a loading screen if isLoading is true,

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { useFetchAlbumsQuery } from '../store';
 import AlbumListItem from './AlbumListItem';
 import SkeletonLoader from './SkeletonLoader';
+import Button from './Button';
 
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
     let renderAlbums = null;
+
+    const handleAddAlbum = () => {
+        console.log("Add Album Request inside " + user.name);
+    }
+
+    const renderAddAlbumBtn = () => {
+        if (isLoading) {
+            return (
+                <div className='flex flex-row items-center' >
+                    <iframe src="https://embed.lottiefiles.com/animation/98432" width="35px" height="35px" className='mr-3' title='Adding a user' ></iframe>
+                    <Button className="bg-gray-700 text-gray-400 border-gray-400" >+ Add Album</Button>
+                </div>
+            )
+        } else {
+            return <Button primary onClick={handleAddAlbum} >+ Add Album</Button>
+        }
+    }
 
     if (!isLoading && !error) {
         renderAlbums = data.map((album) => {
@@ -19,9 +37,15 @@ const AlbumsList = ({ user }) => {
     // component return
     return (
         <div>
-            Albums
+            <div className='flex flex-row justify-between m-2 items-center' >
+                <label className='text-md font-bold' >Albums</label>
+                <div className="text-sm" >
+                    {renderAddAlbumBtn()}
+                </div>
+            </div>
+
             <div>
-                {isLoading ? <SkeletonLoader times={3} heightNwidth="h-16 w-full" /> : error ? error.status + ' || ' + error.error : renderAlbums}
+                {isLoading ? <SkeletonLoader times={3} heightNwidth="h-14 w-full" /> : error ? error.status + ' || ' + error.error : renderAlbums}
             </div>
         </div>
     )
