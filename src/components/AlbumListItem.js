6b70d9a3c8fa94f8b3f4a882fe8e4c4ea8a42530
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-12 21:41:14 +0530

    feat: create delete button in album list items
    This commit creates the delete button on every
    albums. This commit will also create all the
    neccessary logic to invoke a function that
    should delete the album. Deletation data handling
    logics will be updated in next commit.

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import ExpandablePanel from './ExpandablePanel'
+import { GoTrash } from 'react-icons/go'
 
 const AlbumListItem = ({ album }) => {
 
+    const handleDelete = () => {
+        console.log('Deletation request of album - ' + album.name)
+    }
+
     const header =
         <>
-            <div className='rounded'>
+            <div className='rounded flex flex-row items-center'>
+                <div className='cursor-pointer p-2' onClick={handleDelete}>
+                    <GoTrash />
+                </div>
                 <div className='p-1 text-sm' >{album.name}</div>
             </div>
         </>
 
     return (
         <ExpandablePanel header={header} bgcolor='bg-40' >
-            <label className='text-sm' >CONTENT !!!</label>
+            <div className='ml-9'>
+                <label className='text-sm' >Images for album {album.name}</label>
+            </div>
         </ExpandablePanel>
     )
 }
