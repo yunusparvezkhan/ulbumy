e3b75cbe8d7167e58b9c1a967e54bea6fe2eb2e5
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-14 01:20:15 +0530

    feat: add error msging on image deletation errors

--- src/components/ImageListItem.js
+++ src/components/ImageListItem.js
@@ -20,7 +20,7 @@ const ImageListItem = ({ image }) => {
             )
         } else {
             return (
-                <Button danger className='flex flex-row items-center gap-1 mb-4 cursor-pointer text-sm' onClick={handleDelete}>
+                <Button danger className='flex flex-row items-center gap-1 cursor-pointer text-sm' onClick={handleDelete}>
                     <GoTrash /> Delete
                 </Button>
             )
@@ -28,10 +28,13 @@ const ImageListItem = ({ image }) => {
     }
 
     return (
-        <div className='flex flex-col items-center'  >
+        <div className='flex flex-col items-center mb-4'  >
             <img src={image.src} alt={image.src} className='w-40 h-40 mb-2 ' />
             <div>
                 {renderDeleteButton()}
+            </div>
+            <div className='mb-3' >
+                {result.error && <p className='text-red-500 tet-center' >{result.error}</p>}
             </div>
         </div >
     )
