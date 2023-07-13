6e0268e63653823bec49a28980de4b295a0ff935
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-14 01:03:05 +0530

    feat: create delete button for images

--- src/components/ImageListItem.js
+++ src/components/ImageListItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { GoTrash } from 'react-icons/go';
+import Button from './Button';
 
 const ImageListItem = ({ image }) => {
+
+    const handleDelete = () => {
+        // Delete
+        console.log('Delete request from image id ' + image.id)
+    }
+
     return (
         <div className='flex flex-col items-center'  >
-            <img src={image.src} alt={image.alt} className='w-40 h-40' />
-            <div className='text-sm text-center' >{image.alt}</div>
+            <div className='text-sm text-center mb-1' >{image.alt}</div>
+            <img src={image.src} alt={image.alt} className='w-40 h-40 mb-2 ' />
+            <Button danger className='flex flex-row items-center gap-1 mb-4 cursor-pointer text-sm' onClick={handleDelete}>
+                <GoTrash /> Delete
+            </Button>
         </div >
     )
 }
