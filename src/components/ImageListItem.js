440212ca3a7eab3c4a47e7d29f5a358d1cd74be2
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-14 01:17:23 +0530

    feat: add processing effect on image deletation

--- src/components/ImageListItem.js
+++ src/components/ImageListItem.js
@@ -11,12 +11,28 @@ const ImageListItem = ({ image }) => {
         doDeleteImage(image);
     }
 
+    const renderDeleteButton = () => {
+        if (result.isLoading) {
+            return (
+                <Button className='flex flex-row items-center gap-1 mb-4 cursor-pointer text-sm bg-gray-700'>
+                    <GoTrash /> Delete
+                </Button>
+            )
+        } else {
+            return (
+                <Button danger className='flex flex-row items-center gap-1 mb-4 cursor-pointer text-sm' onClick={handleDelete}>
+                    <GoTrash /> Delete
+                </Button>
+            )
+        }
+    }
+
     return (
         <div className='flex flex-col items-center'  >
             <img src={image.src} alt={image.src} className='w-40 h-40 mb-2 ' />
-            <Button danger className='flex flex-row items-center gap-1 mb-4 cursor-pointer text-sm' onClick={handleDelete}>
-                <GoTrash /> Delete
-            </Button>
+            <div>
+                {renderDeleteButton()}
+            </div>
         </div >
     )
 }
